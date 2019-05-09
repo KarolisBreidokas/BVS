a219f0b8aaf81dc62089ede641c90b9253144cfc
KarolisBreidokas <karolis.breidokas@ktu.edu>
2019-05-09 17:34:43 +0300

    Making MVC Stub

---
+++ BVS/BVS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BVS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
