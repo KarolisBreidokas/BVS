963beeceedb79bc1bf2c0cb09d19445404bf5f03
karolissto <ltkarolissto@gmail.com>
2019-05-16 21:02:55 +0300

    Done some work on warehouse subsystem

---
+++ BVS/BVS/Controllers/WarehouseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BVS.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        public IActionResult PartsList()
+        {
+            return View();
+        }
+
+        public IActionResult OrdersPage()
+        {
+            return View();
+        }
+
+        public IActionResult OrderForm()
+        {
+            return View();
+        }
+    }
+}
