94c8eed740ed4f3e94cc6b735fc21f1b343bd7ed
Tadas Kalvaitis <t.kalvaitis@ktu.edu>
2019-05-17 13:11:38 +0300

    ATMRepository

---
+++ BVS/BVSREPOS/Repositories/ATM_Repository.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BVS.Data.DTOs;
+using BVS.Data.Models;
+using BVS.Data.Repositories.Interfaces;
+using DevOne.Security.Cryptography.BCrypt;
+using Microsoft.EntityFrameworkCore;
+
+namespace BVS.Data.Repositories
+{
+    public class ATM_Repository:IATM_Repository
+    {
+        private readonly DbSet<ATM> _ATMs;
+        private readonly BVSDBContext _context;
+
+        public ATM_Repository(BVSDBContext context)
+        {
+            _context = context;
+            _ATMs = _context.ATMs;
+        }
+
+        public async Task changeATMData(int id, NewATMDto atmDto)
+        {
+            var ans = await _ATMs.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (ans is null)
+                throw new NotImplementedException();
+
+            ans.Address = atmDto.Address;
+            ans.AditionalInfo = atmDto.AditionalInfo;
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> createNewATM(NewATMDto atmDto)
+        {
+            var ATMEntity = new ATM
+            {
+                Address = atmDto.Address,
+                AditionalInfo = atmDto.AditionalInfo
+            };
+
+            await _context.SaveChangesAsync();
+            return ATMEntity.Id;
+        }
+
+        public async Task<bool> delete(int id)
+        {
+            var ans = await _ATMs.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (ans is null)
+                throw new NotImplementedException();
+
+            _ATMs.Remove(ans);
+            await _context.SaveChangesAsync();
+
+            return true;            //nzn kaip gaut boola pasalino ar ne
+        }
+
+        /// <summary>
+        /// idk kas cia yra
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ATM> getATM(int id)
+        {
+            var ans = await _ATMs.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (ans is null)
+                throw new NotImplementedException();
+            return ans;
+        }
+
+        public ICollection<ATM> getATMs()
+        {
+            var ans = _ATMs.ToList();
+            if (ans is null)
+                throw new NotImplementedException();
+            return ans;
+        }
+
+        public ICollection<ATM> search(string address)
+        {
+            var ans = _ATMs.Where(x => x.Address.Contains(address)).ToList();
+            if (ans is null)
+                throw new NotImplementedException();
+            return ans;
+        }
+
+        public async Task updateATMState(int atmId, ATM_State state)
+        {
+            var ans = await _ATMs.Where(x => x.Id == atmId).FirstOrDefaultAsync();
+            if (ans is null)
+                throw new NotImplementedException();
+
+            if (state.Equals("InTransit"))
+            {
+                ans.State = state;
+                ans.Online = false;
+            }
+            else
+            {
+                ans.State = state;
+                ans.Online = true;
+            }
+
+            await _context.SaveChangesAsync();
+            
+        }
+    }
+}
--- BVS/BVSREPOS/Repositories/Interfaces/IATM_Repository.cs
+++ BVS/BVSREPOS/Repositories/Interfaces/IATM_Repository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using BVS.Data.DTOs;
 using BVS.Data.Models;
+using System.Threading.Tasks;
 
 namespace BVS.Data.Repositories.Interfaces
 {public interface IATM_Repository
@@ -9,7 +10,7 @@ namespace BVS.Data.Repositories.Interfaces
 		/**
 		 * Atnaujina bankomato dauomenis suranda bankomatą pagal id bus Dto ar viską per parametrus
 		 */
-		void changeATMData(int id,NewATMDto atmDto);        //REIKIA
+		Task changeATMData(int id,NewATMDto atmDto);        //REIKIA
 
         /**
 		 * Paima visus bankomatus is duombazes
@@ -23,7 +24,7 @@ namespace BVS.Data.Repositories.Interfaces
 		 * Paima bankomatų iš duombazės pagal id
 		 */
 
-        ATM getATM( int id);        //REIKIA
+        Task<ATM> getATM( int id);        //REIKIA
 
         /**
 		 * Ieško bankomato pagal adresą
@@ -35,20 +36,20 @@ namespace BVS.Data.Repositories.Interfaces
 		 * Atnaujina specifinio bankomato duomenis parenka pagal id
 		 */
 
-        void updateATMState(int atmId,ATM_State state);     //REIKIA
+        Task updateATMState(int atmId,ATM_State state);     //REIKIA
 
         /**
 		 * Sukuria / ideda nauaj bankomata i duombaze
 		 */
 
-        int createNewATM(NewATMDto atmDto);     //REIKIA
+        Task<int> createNewATM(NewATMDto atmDto);     //REIKIA
 
         /**
 		 * Pašalina bankomato iš duombazės pajima pagal adresą
 		 * adresą????
 		 */
 
-        bool delete(int id);    //REIKIA
+        Task<bool> delete(int id);    //REIKIA
 		
 	}
 	
