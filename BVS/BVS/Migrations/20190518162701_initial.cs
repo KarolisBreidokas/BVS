98eb269cc018a31825afaf81e543373e3cfe0678
KarolisBreidokas <karolis.breidokas@ktu.edu>
2019-05-18 20:41:21 +0300

    commiting DB migration

---
+++ BVS/BVS/Migrations/20190518162701_initial.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BVS.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ATM",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Address = table.Column<string>(nullable: true),
+                    AditionalInfo = table.Column<string>(nullable: true),
+                    State = table.Column<int>(nullable: false),
+                    Online = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ATM", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ATM_Part",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Discriminator = table.Column<string>(nullable: false),
+                    Nominal = table.Column<float>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ATM_Part", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rack",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rack", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Discriminator = table.Column<string>(nullable: false),
+                    PhoneNo = table.Column<string>(nullable: true),
+                    Worker_PhoneNo = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ATM_Transport",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    NewAddress = table.Column<string>(nullable: true),
+                    OrderDate = table.Column<DateTime>(nullable: false),
+                    AtmId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ATM_Transport", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ATM_Transport_ATM_AtmId",
+                        column: x => x.AtmId,
+                        principalTable: "ATM",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ATM_Message",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(nullable: false),
+                    AuthorId = table.Column<int>(nullable: false),
+                    Discriminator = table.Column<string>(nullable: false),
+                    PartId = table.Column<int>(nullable: true),
+                    Body = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ATM_Message", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ATM_Message_ATM_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "ATM",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ATM_Message_ATM_Part_PartId",
+                        column: x => x.PartId,
+                        principalTable: "ATM_Part",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PartInStorage",
+                columns: table => new
+                {
+                    RackId = table.Column<int>(nullable: false),
+                    PartId = table.Column<int>(nullable: false),
+                    Count = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PartInStorage", x => new { x.PartId, x.RackId });
+                    table.ForeignKey(
+                        name: "FK_PartInStorage_ATM_Part_PartId",
+                        column: x => x.PartId,
+                        principalTable: "ATM_Part",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PartInStorage_Rack_RackId",
+                        column: x => x.RackId,
+                        principalTable: "Rack",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(nullable: false),
+                    ArivalDate = table.Column<DateTime>(nullable: false),
+                    AuthorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Order_User_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subscription",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false),
+                    ATMId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscription", x => new { x.UserId, x.ATMId });
+                    table.ForeignKey(
+                        name: "FK_Subscription_ATM_ATMId",
+                        column: x => x.ATMId,
+                        principalTable: "ATM",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Subscription_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserReport",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserReport", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserReport_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Job",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: true),
+                    State = table.Column<int>(nullable: false),
+                    WorkerId = table.Column<int>(nullable: true),
+                    MessageId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Job", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Job_ATM_Message_MessageId",
+                        column: x => x.MessageId,
+                        principalTable: "ATM_Message",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Job_User_WorkerId",
+                        column: x => x.WorkerId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderedPart",
+                columns: table => new
+                {
+                    PartId = table.Column<int>(nullable: false),
+                    OrderId = table.Column<int>(nullable: false),
+                    Price = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderedPart", x => new { x.PartId, x.OrderId });
+                    table.ForeignKey(
+                        name: "FK_OrderedPart_Order_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Order",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderedPart_ATM_Part_PartId",
+                        column: x => x.PartId,
+                        principalTable: "ATM_Part",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Report",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Body = table.Column<string>(nullable: true),
+                    Completed = table.Column<bool>(nullable: false),
+                    JobId = table.Column<int>(nullable: false),
+                    WorkerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Report", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Report_Job_JobId",
+                        column: x => x.JobId,
+                        principalTable: "Job",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Report_User_WorkerId",
+                        column: x => x.WorkerId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ATM_Message_AuthorId",
+                table: "ATM_Message",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ATM_Message_PartId",
+                table: "ATM_Message",
+                column: "PartId",
+                unique: true,
+                filter: "[PartId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ATM_Message_PartId1",
+                table: "ATM_Message",
+                column: "PartId",
+                unique: true,
+                filter: "[PartId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ATM_Transport_AtmId",
+                table: "ATM_Transport",
+                column: "AtmId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Job_MessageId",
+                table: "Job",
+                column: "MessageId",
+                unique: true,
+                filter: "[MessageId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Job_WorkerId",
+                table: "Job",
+                column: "WorkerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_AuthorId",
+                table: "Order",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderedPart_OrderId",
+                table: "OrderedPart",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PartInStorage_RackId",
+                table: "PartInStorage",
+                column: "RackId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Report_JobId",
+                table: "Report",
+                column: "JobId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Report_WorkerId",
+                table: "Report",
+                column: "WorkerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscription_ATMId",
+                table: "Subscription",
+                column: "ATMId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserReport_UserId",
+                table: "UserReport",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ATM_Transport");
+
+            migrationBuilder.DropTable(
+                name: "OrderedPart");
+
+            migrationBuilder.DropTable(
+                name: "PartInStorage");
+
+            migrationBuilder.DropTable(
+                name: "Report");
+
+            migrationBuilder.DropTable(
+                name: "Subscription");
+
+            migrationBuilder.DropTable(
+                name: "UserReport");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "Rack");
+
+            migrationBuilder.DropTable(
+                name: "Job");
+
+            migrationBuilder.DropTable(
+                name: "ATM_Message");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "ATM");
+
+            migrationBuilder.DropTable(
+                name: "ATM_Part");
+        }
+    }
+}
